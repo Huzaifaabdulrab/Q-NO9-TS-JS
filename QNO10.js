090461743f0dbfa0d698b7ae9087bc65f2ca6b99
Huzaifaabdulrab <149306467+Huzaifaabdulrab@users.noreply.github.com>
2024-03-14 00:15:02 +0500

    Add files via upload

---
+++ QNO10.js
@@ -0,0 +1,7 @@
+"use strict";
+// 10 .Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+Object.defineProperty(exports, "__esModule", { value: true });
+let program1 = "HUZAIFA";
+let program2 = `2/17/2024`;
+console.log(`program 1 = ${program1}`);
+console.log(`program 2 = ${program2}`);
