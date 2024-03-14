f69281fcf7a6e41762a20c8f1d2f248ada53dcfa
Huzaifaabdulrab <149306467+Huzaifaabdulrab@users.noreply.github.com>
2024-03-14 05:16:44 +0500

    Delete app.ts

--- app.ts
+++
@@ -1,5 +0,0 @@
-// 9. Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
-
-let  number : number = 2
-
-console.log(`${number} is my favorite numnber beacous its my luckey number`);
