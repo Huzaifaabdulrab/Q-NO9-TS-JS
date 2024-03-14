4b682500a78c6b656fd5661d836a0022d2e75fb6
Huzaifaabdulrab <149306467+Huzaifaabdulrab@users.noreply.github.com>
2024-03-14 05:16:33 +0500

    Delete app.js

--- app.js
+++
@@ -1,3 +0,0 @@
-// 9. Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
-var number = 2;
-console.log("".concat(number, " is my favorite numnber beacous its my luckey number"));
